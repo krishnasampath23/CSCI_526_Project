de10a679fbfeafd45e7700c2ef35fc8160214ece
vineesha98 <vineesha98@gmail.com>
2022-09-23 00:36:58 -0700

    Food Generation
    1) Generated Food randomly.
    2) Created 3 kinds of food
    3) Food destroys after 5 seconds of creation
    4) New Food is generated after every 5 seconds.

---
+++ Assets/FoodSpawn.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodSpawn : MonoBehaviour
+{
+    public GameObject[] foodItem;
+    public Vector3 minValue;
+    public Vector3 maxValue;
+    private List<GameObject> q;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        q = new List<GameObject>();
+        InvokeRepeating("spawnObject",0,5);
+    }
+    void spawnObject()
+    {
+      GameObject item = Instantiate(foodItem[Random.Range(0, foodItem.Length)]);
+      item.transform.position = transform.position + new Vector3(Random.Range(minValue.x,maxValue.x),
+      Random.Range(minValue.y,maxValue.y),0);
+      item.transform.parent = transform;
+      q.Add(item);
+      Object.Destroy(q[0], 5.0f);
+      q.Remove(q[0]);
+    }
+}
