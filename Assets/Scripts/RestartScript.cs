497c9011decc993c745cc71bf962e0bd156c29c4
krishnasampath23 <krishnasampath23@gmail.com>
2022-10-09 13:16:39 -0700

    Added Levels to Menu and Created Empty Level Scenes

--- Assets/Scripts/RestartScript.cs
+++ Assets/Scripts/RestartScript.cs
@@ -16,6 +16,43 @@ public class RestartScript : MonoBehaviour
 
         SceneManager.LoadScene("Scene1");
     }
+
+     public void LoadLevels()
+    {
+        StaticScript.timePrevious=StaticScript.timeElapsed;
+        StaticScript.score = 0;
+        StaticScript.timeLeft = 180;
+        StaticScript.timerOn = false;
+        StaticScript.no_of_poops = 10;
+        StaticScript.health = 100;
+
+        SceneManager.LoadScene("Levels");
+    }
+    public void LoadLevel1()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+    public void LoadLevel2()
+    {
+        SceneManager.LoadScene("Level2");
+    }
+
+    public void LoadLevel3()
+    {
+        SceneManager.LoadScene("Level3");
+    }
+
+    public void LoadLevel4()
+    {
+        SceneManager.LoadScene("Level4");
+    }
+
+    public void LoadBack()
+    {
+        SceneManager.LoadScene("RestartScene");
+    }
+
+
     public void ExitGame()
     {
         Application.Quit();
