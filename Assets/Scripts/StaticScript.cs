543ac939b0a16fe89d65737c88e4525bb01c59f8
Manavjain25 <48240726+Manavjain25@users.noreply.github.com>
2022-10-10 21:28:25 -0700

    blinker changes

--- Assets/Scripts/StaticScript.cs
+++ Assets/Scripts/StaticScript.cs
@@ -11,7 +11,7 @@ static class StaticScript
     public static int _poopsUsed=0;
     public static int enemies_killed=0;
     public static int no_of_enemies = 3;
-    public static float timeLeft = 10;
+    public static float timeLeft = 12;
     public static bool timerOn = false;
     public static int no_of_poops = 10;
     public static int health = 100;
