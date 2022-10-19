70ed646da25582f072e1a2ffcc09c427c68417b4
zhangzhengyuan <zhangzhengyuan0604@gmail.com>
2022-10-18 21:05:08 -0700

    increase no_of_poops to 10

--- Assets/Scripts/StaticScript.cs
+++ Assets/Scripts/StaticScript.cs
@@ -14,7 +14,7 @@ static class StaticScript
     public static int no_of_enemies = 3;
     public static float timeLeft = 0;
     public static bool timerOn = false;
-    public static int no_of_poops = 0;
+    public static int no_of_poops = 10;
     public static int health = 100;
     public static int level = 0;
     public static int success_or_fail=-1;
