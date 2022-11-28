cbc857c44b3fa5ddb11863317a821c9492e9ca94
zhangzhengyuan <zhangzhengyuan0604@gmail.com>
2022-11-28 15:18:40 -0800

    simplify EnemyBullet.cs

--- Assets/Scripts/EnemyBullet.cs
+++ Assets/Scripts/EnemyBullet.cs
@@ -1,62 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyBullet : MonoBehaviour
-{   
+{
     private float bulletSpeed = 100f;
     private float targetTime = 10.0f;
-    Vector2 lastVelocity;
     private Rigidbody2D rb;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         rb.velocity = Vector2.up * bulletSpeed;
-        lastVelocity = rb.velocity;
-    }
-
-    private void FixedUpdate(){
-        //rb.velocity = Vector2.right * bulletSpeed;
-        // // rb.velocity = Vector2.down * bulletSpeed;
-        // rb.velocity.x += bulletSpeed;
-        // rb.velocity.y += bulletSpeed;
-        // if(isref == true){
-        //     // rb.velocity *= bulletSpeed*10;
-        //     rb.AddForce(transform.position * bulletSpeed);
-        //     isref = false;
-        // }
-        lastVelocity = rb.velocity;
-        targetTime -= Time.deltaTime;
-        if (targetTime <= 0.0f)
-        {
-            Destroy(this.gameObject);
-        }
+        Destroy(this.gameObject, targetTime);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Line"))
         {
-            rb.velocity = Vector2.Reflect(lastVelocity, collision.contacts[0].normal);
-            // if(isref==false){
-            //     rb.velocity = Vector2.Reflect(lastVelocity, collision.contacts[0].normal);
-            //     isref = true;
-            // }
-            
+            rb.velocity = Vector2.Reflect(rb.velocity, collision.contacts[0].normal);
         }
-        // else if (collision.gameObject.CompareTag("LevelStart"))
-        // {
-        //     rb.velocity = Vector2.Reflect(lastVelocity, collision.contacts[0].normal);
-        //     // if(isref==false){
-        //     //     rb.velocity = Vector2.Reflect(lastVelocity, collision.contacts[0].normal);
-        //     //     isref = true;
-        //     // }
-        // }
-        else {
+        else
+        {
             Destroy(this.gameObject);
         }
-
-        
     }
 }
